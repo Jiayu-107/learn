2358e1e9e171a437c01ee3b752e70850fd2b69d7
Jiayu-107 <8849440@qq.com>
2022-04-11 20:35:51 +0800

    冒泡排序函数

---
+++ test_411_1/test.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void bubble_sort(int arr[], int sz)
+//void bubble_sort(int* arr,int sz) It is fine too.
+{
+	//确定冒泡排序的趟数；
+
+	int i = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		//确定冒泡排序的每趟比较的次数；
+
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int tmp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = tmp;
+
+			}
+		}
+	}
+	
+}
+int main()
+{
+	//实现冒泡排序法；
+	int i = 0;
+	int arr[] = {9,10,7,6,4,5,3,2,1,8,0};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	//对arr进行排序，排成升序；
+	//arr是数组，我们对数组进行传参，实际上传过去的是数组首元素的地址 &arr[0]
+	bubble_sort(arr, sz);//冒泡排序函数
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	
+	return 0;
+}
