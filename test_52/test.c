489aebf132100df2281b17b6e85ef559d8347e3c
Jiayu-107 <8849440@qq.com>
2022-05-02 11:19:02 +0800

    整型指针数组

---
+++ test_52/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	//整型指针数组
+
+	int arr1[] = {1,2,3,4,5};
+	int arr2[] = {2,3,4,5,6};
+	int arr3[] = {3,4,5,6,7};
+
+	int* parr[] = { arr1,arr2,arr3 };
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 5; j++)
+		{
+			printf("%d ", *(parr[i] + j));
+		}
+		printf("\n");
+	}
+	return 0;
+}
