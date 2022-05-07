737d578db74f76593ebed8aa821b11d6a8d8e342
Jiayu-107 <8849440@qq.com>
2022-05-07 17:30:47 +0800

    指针习题

---
+++ test_57/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	//Ò»Î¬Êý×é
+	int a[] = { 1,2,3,4 };
+
+	printf("%d\n", sizeof(a));// 16
+	printf("%d\n", sizeof(a + 0));// 4/8
+	printf("%d\n", sizeof(*a));// 4
+	printf("%d\n", sizeof(a + 1));// 4/8
+	printf("%d\n", sizeof(a[1]));// 4
+	printf("%d\n", sizeof(&a));// 4/8
+	printf("%d\n", sizeof(*&a));// 4
+	printf("%d\n", sizeof(&a + 1));// 4/8
+	printf("%d\n", sizeof(&a[0]));// 4/8
+	printf("%d\n", sizeof(&a[0] + 1));// 4/8
+
+	return 0;
+}
