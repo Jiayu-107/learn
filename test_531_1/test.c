7f02386cc72f1970c57674c69b1421daca7994cf
Jiayu-107 <8849440@qq.com>
2022-05-31 23:15:37 +0800

    * C

---
+++ test_531_1/test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "stdio.h"
+int main()
+{
+    printf("ÓÃ * ºÅÊä³ö×ÖÄ¸ C!\n");
+    printf(" ****\n");
+    printf(" *\n");
+    printf(" * \n");
+    printf(" ****\n");
+}
