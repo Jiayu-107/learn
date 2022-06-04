09dc6a3a3cbd4189bbaba027c37ebf650a0137b9
Jiayu-107 <8849440@qq.com>
2022-06-04 23:21:28 +0800

    宏offsetof使用

---
+++ test_0604_1/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stddef.h>
+
+struct S
+{
+	char c1;
+	int a;
+	char c2;
+};
+
+int main()
+{
+	//struct S s;
+	printf("%d\n", offsetof(struct S,c1));
+	printf("%d\n", offsetof(struct S,a));
+	printf("%d\n", offsetof(struct S,c2));
+
+	return 0;
+}
