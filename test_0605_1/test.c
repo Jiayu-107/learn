4f323c6da760b8e81b13a665fdf8e797ae5d51eb
Jiayu-107 <8849440@qq.com>
2022-06-05 20:06:05 +0800

    输出特殊图形

---
+++ test_0605_1/test.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+int main()
+{
+    char a = 176, b = 219;
+    printf("%c%c%c%c%c\n", b, a, a, a, b);
+    printf("%c%c%c%c%c\n", a, b, a, b, a);
+    printf("%c%c%c%c%c\n", a, a, b, a, a);
+    printf("%c%c%c%c%c\n", a, b, a, b, a);
+    printf("%c%c%c%c%c\n", b, a, a, a, b);
+    return 0;
+}
