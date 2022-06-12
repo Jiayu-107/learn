25711cf0f3159e83f7dbc118a05b95dcc570e3a0
Jiayu-107 <8849440@qq.com>
2022-06-12 23:14:19 +0800

    递归阶乘

---
+++ test_0612_1/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int fact(int);
+    for (i = 0; i < 6; i++) {
+        printf("%d!=%d\n", i, fact(i));
+    }
+}
+int fact(int j)
+{
+    int sum;
+    if (j == 0) {
+        sum = 1;
+    }
+    else {
+        sum = j * fact(j - 1);
+    }
+    return sum;
+}
