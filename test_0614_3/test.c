64f2687349b0207f86b3f6320bc242a4a377a392
Jiayu-107 <8849440@qq.com>
2022-06-14 22:19:22 +0800

    求矩阵对角线之和

---
+++ test_0614_3/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#define N 3
+int main()
+{
+    int i, j, a[N][N], sum = 0;
+    printf("请输入矩阵(3*3)：\n");
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+            scanf("%d", &a[i][j]);
+    for (i = 0; i < N; i++)
+        sum += a[i][i];
+    printf("对角线之和为：%d\n", sum);
+    return 0;
+}
