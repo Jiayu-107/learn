d8543c6ef0ca6fc2e867c0c22036bd99836f0227
Jiayu-107 <8849440@qq.com>
2022-06-25 22:47:08 +0800

    结构体变量传递

---
+++ test_0625_4/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+struct student
+{
+    int x;
+    char c;
+} a;
+
+int main()
+{
+    a.x = 3;
+    a.c = 'a';
+    f(a);
+    printf("%d,%c", a.x, a.c);
+}
+f(struct student b)
+{
+    b.x = 20;
+    b.c = 'y';
+}
