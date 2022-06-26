ffc4b3f082ca7e0973936c5afd3c911b297c2d46
Jiayu-107 <8849440@qq.com>
2022-06-26 21:39:11 +0800

    时间函数

---
+++ test_0626_2/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t rawtime;
+    struct tm* timeinfo;
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    printf("当前本地时间为: %s", asctime(timeinfo));
+
+    return 0;
+}
