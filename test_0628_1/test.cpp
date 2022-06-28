9980dec3e902d5c7f659ab8a9b4739eeae9d24e9
Jiayu-107 <8849440@qq.com>
2022-06-28 21:38:23 +0800

    hello world

---
+++ test_0628_1/test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "hello world" << endl;
+
+	//return 将其后的表达式结果或数值 作为函数的返回值返回
+	//return 结束当前函数
+	return 0;
+}
