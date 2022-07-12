993cbe996541c8bd0c0308cf95397b54d93308ec
Jiayu-107 <8849440@qq.com>
2022-07-12 22:46:54 +0800

    指针数组

---
+++ test_0712_4/test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+int main(int argc, char* argv)
+{	
+	//Ö¸ÕëÊý×é
+	const char* arr[4] = { "hehehhe", "xixixixi","hahahaha","enenenen" };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		cout << arr[i] << endl;
+		cout << *(arr[i] + 1) << endl;
+	}
+
+	return 0;
+}
