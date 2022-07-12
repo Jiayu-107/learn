c0117c4a1634c1895712daca48ce5aa050c211d5
Jiayu-107 <8849440@qq.com>
2022-07-12 22:22:57 +0800

    调试技巧

---
+++ test_0712_1/test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char* argv)
+{
+	int* p = NULL;
+	cout << "---001---" << endl;
+	cout << *p << endl;
+	cout << "---002---" << endl;
+
+	return 0;
+}
