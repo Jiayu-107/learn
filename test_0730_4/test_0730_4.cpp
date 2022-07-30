b45b37e91eb8a59f6acc95f58d48c0f2d45155eb
Jiayu-107 <8849440@qq.com>
2022-07-30 23:06:01 +0800

    在set容器中插入自定义类型，并使用仿函数指定排序方式

---
+++ test_0730_4/test_0730_4.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<string>
+#include<set>
+using namespace std;
+
+class SortSetPerson;
+class person
+{
+	friend class SortSetPerson;
+	friend void PrintSetPerson(set<person, SortSetPerson>& s);
+private:
+	int num;
+	string name;
+	float score;
+public:
+	person(){}
+	person(int num, string name, float score)
+	{
+		this->num = num;
+		this->name = name;
+		this->score = score;
+	}
+};
+
+//仿函数：重载函数调用运算符（）的类
+class SortSetPerson
+{
+public:
+	bool operator()(person ob1, person ob2) const
+	{
+		return ob1.num < ob2.num;
+	}
+};
+
+void PrintSetPerson(set<person, SortSetPerson>& s)
+{
+	set<person, SortSetPerson>::iterator it = s.begin();
+	for (; it != s.end(); it++)
+	{
+		cout << (*it).num << (*it).name << (*it).score << endl;
+	}
+}
+int main(int argc, char* argv[])
+{
+	//set容器 存放自定义数据必须修改排序类型
+	set<person, SortSetPerson> s1;
+	s1.insert(person(100, "lucy", 88.8f));
+	s1.insert(person(105, "bob", 88.8f));
+	s1.insert(person(103, "tom", 88.8f));
+	s1.insert(person(104, "mike", 88.8f));
+	s1.insert(person(101, "frank", 88.8f));
+	PrintSetPerson(s1);
+	return 0;
+}
