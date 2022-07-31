7b98f4d2f93cd6d5aa1ee4edac5c58ee1133896d
Jiayu-107 <8849440@qq.com>
2022-07-31 22:35:21 +0800

    一元谓词

---
+++ test_0731_2/test_0731_2.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+bool greaterThan30(int value)
+{
+	return value > 30;
+}
+class GreaterThan30
+{
+public:
+	bool operator()(int value)
+	{
+		return value > 30;
+	}
+};
+int main()
+{
+	vector<int> v1;
+	v1.push_back(10);
+	v1.push_back(30);
+	v1.push_back(50);
+	v1.push_back(70);
+	v1.push_back(90);
+	//find_if条件查找
+	vector<int>::iterator ret;
+	//普通函数提供策略 函数名
+	//ret = find_if(v1.begin(), v1.end(), greaterThan30);
+	//仿函数提供策略 类名称+（）
+	ret = find_if(v1.begin(), v1.end(), GreaterThan30());
+	if (ret != v1.end())
+	{
+		cout << "寻找的结果:" << *ret << endl;
+	}
+}
