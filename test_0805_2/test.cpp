c0e207c0357ed948f7daedf9d350786135a1ae8c
Jiayu-107 <8849440@qq.com>
2022-08-05 10:53:14 +0800

    输出迭代器

---
+++ test_0805_2/test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<iterator>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(40);
+	v.push_back(50);
+
+	copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+	return 0;
+}
