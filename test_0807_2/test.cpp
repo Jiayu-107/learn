082cfdb29cd2def8df3eaa65bd00f322d8a45f78
Jiayu-107 <8849440@qq.com>
2022-08-07 23:03:31 +0800

    计算自然数之和

---
+++ test_0807_2/test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+
+    cout << "输入一个正整数: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    cout << "Sum = " << sum;
+    return 0;
+}
