fcd17225827a112ed4745c2317cc14d67d9723fa
Jiayu-107 <8849440@qq.com>
2022-08-09 23:04:49 +0800

    创建各类三角形图案

---
+++ test_0809_1/test.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+
+    cout << "ÊäÈëÐÐÊý: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; ++i)
+    {
+        for (int j = 1; j <= i; ++j)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
